a40e984263c3194684f47850c60a07f2a81e5dad
Guillaume Tremblay <32100830+thecanadianroot@users.noreply.github.com>
2024-01-08 13:10:37 -0500

    feat: auto merge dependabot PRs (#14)
    * feat: auto merge dependabot PRs
    
    * feat: build multi platforms
    
    * fix: platform build
    
    * fix: dependencies
    
    * fix: load
    
    * fix: unknown platform
    
    * fix: docker image test
    
    * fix: test + platforms
    
    * fix: name
    
    * fix: load
    
    * fix: test script
    
    * update main CI
    
    * fix: add 5 second delay before test

---
+++ .github/workflows/scripts/test-toolkit-docker-image.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sleep 5
+
+URL="http://localhost:8443"
+
+for i in {1..3}; do
+status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -L $URL)
+
+if [[ $status_code -eq 200 ]]; then
+    echo "URL returned 200 OK on attempt $i"
+    break
+else
+    echo "Attempt $i: URL did not return 200 OK (Status: $status_code)"
+    if [[ $i -lt 3 ]]; then
+    echo "Retrying..."
+    else
+    echo "Max retries reached. Exiting."
+    exit 1
+    fi
+fi
+
+sleep 10
+done
